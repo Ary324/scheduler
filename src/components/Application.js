3d7ab2653820e569b2b2f34944037b6f3d53088f
Arian Abedanzadeh <arianabedan@gmail.com>
2021-09-05 12:24:34 -0400

    Update application.js with state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,75 +6,84 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
-const appointments = [
-  {
-    id: 1,
-    time: "1pm",
-  },
-  {
-    id: 2,
-    time: "2pm",
-    interview: {
-      student: "Jeff Kaplan",
-      interviewer: {
-        id: 1,
-        name: "Selena Sizer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "3pm"
-  },
-  {
-    id: 4,
-    time: "4pm",
-    interview: {
-      student: "Corinna Soroy",
-      interviewer: {
-        id: 3,
-        name: "Muhummad Malzahar",
-        avatar: "https://i.imgur.com/T2WwVfS.png"
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "5pm",
-    interview: {
-      student: "Mitch Jones",
-      interviewer: {
-        id: 4,
-        name: "Corinna Soroy",
-        avatar: "https://i.imgur.com/FK8V841.jpg"
-      }
-    }
-  },
-  {
-    id: "last",
-    time: "6pm",
-    interview: {
-      student: "Carlie Cop",
-      interviewer: {
-        id: 4,
-        name: "Corinna Soroy",
-        avatar: "https://i.imgur.com/FK8V841.jpg"
-      }
-    }
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "1pm",
+//   },
+//   {
+//     id: 2,
+//     time: "2pm",
+//     interview: {
+//       student: "Jeff Kaplan",
+//       interviewer: {
+//         id: 1,
+//         name: "Selena Sizer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "3pm"
+//   },
+//   {
+//     id: 4,
+//     time: "4pm",
+//     interview: {
+//       student: "Corinna Soroy",
+//       interviewer: {
+//         id: 3,
+//         name: "Muhummad Malzahar",
+//         avatar: "https://i.imgur.com/T2WwVfS.png"
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "5pm",
+//     interview: {
+//       student: "Mitch Jones",
+//       interviewer: {
+//         id: 4,
+//         name: "Corinna Soroy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg"
+//       }
+//     }
+//   },
+//   {
+//     id: "last",
+//     time: "6pm",
+//     interview: {
+//       student: "Carlie Cop",
+//       interviewer: {
+//         id: 4,
+//         name: "Corinna Soroy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg"
+//       }
+//     }
+//   }
+// ];
 
-const appointmentItem = appointments
-  .map(appointment => {
-    return <Appointment key={appointment.id} {...appointment} />
-  });
+// const appointmentItem = appointments
+//   .map(appointment => {
+//     return <Appointment key={appointment.id} {...appointment} />
+//   });
 
 
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState('Monday');
+  // const [days, setDays] = useState([]);
+  // const [day, setDay] = useState('Monday');
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     const url = '/api/days';
@@ -93,7 +102,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -102,7 +111,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointmentItem}
+        {/* {appointmentItem} */}
       </section>
     </main>
   );
