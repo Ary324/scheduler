3e61a5bc86bd4cb286be8e9d1513513861a3d7b5
Arian Abedanzadeh <arianabedan@gmail.com>
2021-09-22 12:10:10 -0400

    Fix problem with interview not being updated on browser

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,30 +6,60 @@ import Button from "components/Button";
 
 
 export default function Form(props) {
-  const { onCancel, onSave, interviewers } = props;
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank!");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
+  };
+
   return (
-    <main className="appointment__card appointment__card--create">
-      <section className="appointment__card-left">
-        <form autoComplete="off">
+    <main className='appointment__card appointment__card--create'>
+      <section className='appointment__card-left'>
+        <form autoComplete='off' onSubmit={(event) => event.preventDefault()}>
           <input
-            className="appointment__create-input text--semi-bold"
-            name="name"
-            type="text"
-            placeholder="Enter Student Name"
+            className='appointment__create-input text--semi-bold'
+            name='name'
             value={name}
+            type='text'
+            placeholder='Enter Student Name'
             onChange={(event) => setName(event.target.value)}
+            data-testid='student-name-input'
           />
         </form>
-        <InterviewerList interviewers={interviewers} value={interviewer} onChange={setInterviewer} />
+        <section className='appointment__validation'>{error}</section>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
       </section>
-      <section className="appointment__card-right">
-        <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
-          <Button confirm onClick={onSave}>Save</Button>
+      <section className='appointment__card-right'>
+        <section className='appointment__actions'>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={validate}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
-  )
+  );
 }
